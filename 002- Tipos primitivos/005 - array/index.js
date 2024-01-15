6e38ff1acb4ab49a46973a005521a8d075cd1e60
Bianca Gabriel <71979485+biancagabriel@users.noreply.github.com>
2024-01-15 09:17:29 -0300

    array e object

--- 002- Tipos primitivos/005 - array/index.js
+++ 002- Tipos primitivos/005 - array/index.js
@@ -1,4 +1,5 @@
 console.log(["bianca","gabriel"].length)
 console.log(["bianca","gabriel"])
 
-//cria lista de dados, da para por todos os tipos de dados, chave-valor, mas só usa o valor 
+/*cria lista de dados, da para por todos os tipos de dados, chave-valor, mas só usa o valor
+.length: mostra o tamanho  */
---
+++ 002- Tipos primitivos/006 - object/index.js
@@ -0,0 +1,20 @@
+/* objeto tem propriedade, atributos e funcionalidades 
+pode ter um array, numero, boolean*/
+
+console.log([
+    {
+        tenis: {
+            preco: 150,
+            disponivel: true,
+            estoque: 150,
+            cadarco: {
+            cor: "rosa",
+            tamanho: "G"
+            },
+            palmilha: {
+                tamanho : 43,
+                cor: "roxo",
+            },
+        },
+    }
+])
